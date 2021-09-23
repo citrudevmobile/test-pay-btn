5f2324f3a371348baa9900d09738115b29795053
citrudevmobile <citrudevmobile@gmail.com>
2021-09-23 09:53:51 -0400

    save changes

--- src/SplitBtn/SplitBtnView.tsx
+++ src/SplitBtn/SplitBtnView.tsx
@@ -5,5 +5,5 @@ export interface SplitBtnViewProps {
 }
 
 export default React.memo<SplitBtnViewProps>(()=> {
-  return <div> <div style={{ backgroundColor: "pink", display: "inline" }}>{"< P >"}</div> &nbsp&nbsp <div style={{ backgroundColor: "pink", display: "inline" }}>{"< P >"}</div> </div>;
+  return <div> <div style={{ backgroundColor: "pink", display: "inline", margin: "10px" }}>{"< P >"}</div> <div style={{ backgroundColor: "pink", display: "inline", margin: "10px" }}>{"< B >"}</div> </div>;
 });
