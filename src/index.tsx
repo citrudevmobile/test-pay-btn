f745c681e8d715e644431c4814c52fc4e0144be6
citrudevmobile <citrudevmobile@gmail.com>
2021-09-23 10:00:57 -0400

    save changes

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-// 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
+// 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
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
