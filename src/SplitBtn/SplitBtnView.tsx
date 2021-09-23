43b94d208d9adce99dd91c05a0e88cbe22e762d9
citrudevmobile <91247184+citrudevmobile@users.noreply.github.com>
2021-09-23 03:31:42 -0700

    adding split buttons

---
+++ src/SplitBtn/SplitBtnView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface SplitBtnViewProps {
+  // Fill here
+}
+
+export default React.memo<SplitBtnViewProps>(()=> {
+  return <div style={{ backgroundColor: "pink" }}>{"< P >"}</div> <div style={{ backgroundColor: "pink" }}>{"< B >"}</div>;
+});
