de007acdb40c3f7259490b2da94511b0a5c13226
citrudevmobile <citrudevmobile@gmail.com>
2021-09-25 09:08:44 -0400

    save chnages

--- src/PayBtn/PayBtn.tsx
+++ src/PayBtn/PayBtn.tsx
@@ -8,6 +8,7 @@ import { fetchInvoiceFrom } from "../actions";
 import PayBtnView from "./PayBtnView";
 import SplitBtnView from "./SplitBtnView";
 import ShowLoading from "./LoadingBtnView";
+import PayTextbox from "./PayTextboxView"
 
 export interface PayBtnProps {
   amount: number;
@@ -19,6 +20,7 @@ export default React.memo<PayBtnProps>(function PayBtn(props) {
     const [showPayBtn, setShowPayBtn] = React.useState('block')
     const [showSplitBtn, setShowSplitBtn] = React.useState('none')
     const [showLoading, setShowLoading] = React.useState('none')
+    const [showPayTextbox, setShowPayTextbox] = React.useState('none')
 
     const payBtnClicked = () => {
         setShowPayBtn('none')
@@ -30,6 +32,7 @@ export default React.memo<PayBtnProps>(function PayBtn(props) {
         setShowLoading('block')
         setTimeout(()=>{
            setShowLoading('none') 
+           setShowPayTextbox('block')
         },4000)
     }
 
@@ -38,6 +41,7 @@ export default React.memo<PayBtnProps>(function PayBtn(props) {
         setShowLoading('block')
         setTimeout(()=>{
            setShowLoading('none') 
+           setShowPayTextbox('block')
         },4000)
     }
 
@@ -46,6 +50,7 @@ export default React.memo<PayBtnProps>(function PayBtn(props) {
         <PayBtnView payBtnClicked={payBtnClicked} showPayBtn={showPayBtn}/> 
         <SplitBtnView showSplitBtn={showSplitBtn} paypal={paypal} bitcoin={bitcoin}/> 
         <ShowLoading showLoading={showLoading}/>
+        <PayTextbox showPayTextbox={showPayTextbox} />
     </div>
   );
 });
